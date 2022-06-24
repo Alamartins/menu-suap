1e23eb00db43ca97bb1ae7e1fa7720124f9ba064
Alamartins <nunesalan099@gmail.com>
2022-06-24 13:21:13 -0300

    Adicionando componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,36 @@
 import Title from './Title';
 import Noticia from './Noticia';
-import Button from './Button';
+// import Button from './Button';
 import "./App.css";
 function App() {
     const lista_noticia =[
       {
-        horario: "01/06/2022 8:13",
-        descricao: "NOTA À COMUNIDADE: Bloqueio do orçamento da Educação",
+        name: "Start",
+        descricao: "Free",
+        users: "1 Users",
+        project: "1 Project"
       },
       {
-        horario:"31/05/2022 12:35",
-        descricao:"IFRO publica edital para Cadastro de Interesse de Remoção",
-
+        name:"Basic",
+        descricao:"$19.99",
+        users: "5 Users",
+        project: "20 Projects"
       },
       {
-        horario:"30/05/2022 09:54",
-        descricao:"Parceria entre IFRO e UAB irá ampliar a oferta de cursos de pós graduação e de Pedagogia",
-      },
-      {
-        horario:"25/05/2022 15:10",
-        descricao:"Manutenção do Sistema SUAP acontece nesta sexta-feira (27)",
-      },
-      {
-        horario:"25/05/2022 07:44",
-        descricao:"Pais e responsáveis por alunos podem ter acesso a informações estudantis pelo App IFRO Mobile",
+        name:"Expert",
+        descricao:"$129.99",
+        users: "Unlimited Users",
+        project: "Unlimited Projects"
       },
     ]
     return(
       <div className='container'>           
       <Title />
       {lista_noticia.map((noticia)=>{
-        return<Noticia horario={noticia.horario} desc={noticia.descricao}/>
+        return<Noticia name={noticia.name} desc={noticia.descricao} users={noticia.users} project={noticia.project}/>
       })}
 
-      <Button frase="MAIS NOTÍCIAS"/>
+      {/* <Button frase="MAIS NOTÍCIAS"/> */}
       </div>
     ); 
 }
