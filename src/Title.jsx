e00f4c7eef1736f1d97c791c560142544d1c1d5e
Alamartins <nunesalan099@gmail.com>
2022-06-24 13:18:02 -0300

    Adicionando componente Title

--- src/Title.jsx
+++ src/Title.jsx
@@ -2,8 +2,8 @@ import "./Title.css";
 function Title(){
     return(
         <div>
-            <p className="title">NOTÍCIAS</p>
-            <p className="subtitle">PORTAL DO IFRO</p>
+            <p className="title">Premium Plans</p>
+            <p className="subtitle">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
         </div>
     );
 }
